25e5f1f9cd79c9541679838a0ccf6e081c8ea190
Laimonas Jočys <laimonas.jocys@ruptela.com>
2021-07-28 14:33:29 +0300

    code format

--- src/features/clients/components/FormTextField.tsx
+++ src/features/clients/components/FormTextField.tsx
@@ -1,7 +1,7 @@
-import {TextField} from '@material-ui/core';
-import {FieldAttributes, useField} from 'formik';
-import React from 'react';
-import {TextFieldProps} from '@material-ui/core/TextField/TextField';
+import { TextField } from "@material-ui/core";
+import { FieldAttributes, useField } from "formik";
+import React from "react";
+import { TextFieldProps } from "@material-ui/core/TextField/TextField";
 
 const FormTextField = (props: FieldAttributes<TextFieldProps>) => {
   const [field, meta] = useField(props);
